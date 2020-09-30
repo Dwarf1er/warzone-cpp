8618c5cc083dfe0b4106c6b73b7706a3d9db0035
tppham94 <43486951+tppham94@users.noreply.github.com>
2020-09-30 01:28:46 -0400

    start of multiple version

--- Map.cpp
+++ Map.cpp
@@ -1,2 +1,12 @@
 #include "Map.h"
 
+Map::Map()
+{
+
+}
+
+void Map::addContinentEdge(std::vector<std::string> adj[], std::string continent1, std::string continent2)
+{
+	adj[continent1].push_back(continent2);
+	adj[continent2].push_back(continent1);
+}
--- Map.h
+++ Map.h
@@ -2,9 +2,25 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <string>
+#include <vector>
+
 class Map
 {
 
+public:
+	Map();
+	Map(std::string continent1, std::string continent2);
+
+	void addContinentEdge(std::vector<std::string> adj[], std::string continent1, std::string continent2);
+
+	std::vector<std::string> getContinents();
+	std::vector<std::string> getCountries();
+
+	std::vector<std::string> getContinentsConnections();
+	std::vector<std::string> getCountriesConnections();
+private:
+	int vertices;
 };
 
 #endif //!map
