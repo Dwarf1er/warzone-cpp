5bdf5d9749ae56c3cacb73c7d17ec899a573f433
tppham94 <43486951+tppham94@users.noreply.github.com>
2020-10-11 18:19:04 -0400

    get rid of empty lines

--- MapLoader.cpp
+++ MapLoader.cpp
@@ -26,36 +26,31 @@ void loadmap() {
 	bool bordercheck = false;
 	bool mapcheck = false;
 
-	
-	
 	graph.initList();
-
-
 
 	// extracting words from the file 
 	int error = 0;
 	while (infile >> s)
 	{
-
-		int more = 0;  
+		int more = 0;
 
 		if (!s.compare("[continents]")) {
 			continentcheck = true;
 			int g = 0;
-			while(check) {
+			while (check) {
 				if (g >= 100) {
 					cout << "major";
 					break;
 				}
 				g++;
-				
+
 				infile >> s;
 				if (!s.compare("[countries]")) {
 					break;
 				}
 
 				string name = s;
-			   
+
 				//num
 				infile >> x;
 				int num = x;
@@ -63,14 +58,10 @@ void loadmap() {
 				graph.createContinent(name, more);
 				more++;
 				infile >> s;  //color ignorer
-				
-			
-				
+
 			}
-			
 		}
-		
-		
+
 		if (continentcheck == true) {
 			if (!s.compare("[countries]")) {
 				countrycheck = true;
@@ -80,23 +71,21 @@ void loadmap() {
 				int add = 1;
 				int o = 0;
 				while (check) {
-					if (o>=100) {
+					if (o >= 100) {
 						break;
 					}
-					
+
 					infile >> s;   //gets the country number
 					if (!s.compare("[borders]")) {
 						break;
 					}
 
-					
 					if (s.empty()) {
 						cout << "major ";
 						break;
 					}
 
 					infile >> s;
-
 
 					if (s.empty()) {
 						cout << "major";
@@ -114,13 +103,10 @@ void loadmap() {
 						cout << "major ";
 						break;
 					}
-					
+
 					add++;
 					infile >> s;
 
-
-
-
 					if (s.empty()) {
 						cout << "major";
 						break;
@@ -128,27 +114,19 @@ void loadmap() {
 
 					infile >> s;
 
-
 					if (s.empty()) {
 						cout << "major";
 						break;
 					}
-					
+
 					o++;
 
 				}
 			}
-			
-		}
-		
-		
-		
-		
-		
-	
-		
 
-		if (!s.compare("[borders]")) {        
+		}
+
+		if (!s.compare("[borders]")) {
 			if (countrycheck == true) {
 				string::iterator a;
 				string::iterator first;
@@ -158,7 +136,7 @@ void loadmap() {
 				bordercheck = true;
 				int u = 0;
 				while (check) {
-					
+
 					if (u >= 100) {
 						cout << "major error";
 						break;
@@ -166,36 +144,28 @@ void loadmap() {
 
 					u++;
 
-
 					getline(infile, s);
 
 					if (s.empty()) {
-						
+
 						break;
 					}
 
-
 					first = s.begin();
-
 
 					for (a = s.begin() + 1;a < s.end();a++) {
 
 						if (*a != ' ') {
 							graph.addEdge(graph.nodeList[*first], graph.nodeList[*a]);
 						}
-
-
-
 					}
-					
 				}
-				
 			}
 		}
 		error++;
-			if (error >= 200) {
-				cout << "major error";
-				break;
+		if (error >= 200) {
+			cout << "major error";
+			break;
 		}
 
 		//check errors
@@ -204,7 +174,7 @@ void loadmap() {
 			break;
 		}
 	}
-	
+
 	for (int i = 0; i < 1; i++) {
 		if (continentcheck == false) {
 			cout << "error1: continent section invalid" << endl;
@@ -224,10 +194,6 @@ void loadmap() {
 
 	}
 
-
-	
-	
-	
 	if (continentcheck == true && countrycheck == true && bordercheck == true) {
 		graph.printBoard();
 	}
