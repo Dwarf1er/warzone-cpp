eb3824f26dd717e30041f65970208c866430ef43
tppham94 <43486951+tppham94@users.noreply.github.com>
2020-10-12 22:58:19 -0400

    update file maploader

--- WarzoneDriver.cpp
+++ WarzoneDriver.cpp
@@ -22,17 +22,17 @@ int main()
 
     // Part 3 - Player
     printf("======================================= Part 3 =======================================\n");
-    PlayerDriver playerDriver;
+    //PlayerDriver playerDriver;
     printf("======================================= Part 3 end =======================================\n");
     
     // Part 4 - OrdersList
     printf("======================================= Part 4 =======================================\n");
-    OrdersDriver orderDriver;
+    //OrdersDriver orderDriver;
     printf("======================================= Part 4 end =======================================\n");
     
     // Part 5 - Cards
     printf("======================================= Part 5  =======================================\n");
-    CardsDriver cardDriver;
+    //CardsDriver cardDriver;
     printf("======================================= Part 5 end =======================================\n");
     
     auto result = _getch();
