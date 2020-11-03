88c68de1e9d890e56d739c8135e1fdbda4ddb04b
tppham94 <43486951+tppham94@users.noreply.github.com>
2020-11-02 21:57:22 -0500

    Added option to list map files

--- WarzoneDriver.cpp
+++ WarzoneDriver.cpp
@@ -6,35 +6,53 @@
 #include "OrdersDriver.h"
 #include "CardsDriver.h"
 
+//Test To check file in directory 
+#include <filesystem>
+namespace fs = std::filesystem;
+
 //Main method that contains all part to run at once
 int main()
 {
-    // Part 1 - Map
-    printf("======================================= Part 1 =======================================\n");
-    MapDriver mapDriver;
-    printf("======================================= Part 1 End =======================================\n");
+	/*
+	// Part 1 - Map
+	printf("======================================= Part 1 =======================================\n");
+	MapDriver mapDriver;
+	printf("======================================= Part 1 End =======================================\n");
 
-    // Part 2 - MapLoader
-    printf("======================================= Part 2 =======================================\n");
-    MapLoaderDriver mapLoaderDriver;
+	// Part 2 - MapLoader
+	printf("======================================= Part 2 =======================================\n");
+	MapLoaderDriver mapLoaderDriver;
 
-    printf("======================================= Part 2 End =======================================\n");
+	printf("======================================= Part 2 End =======================================\n");
 
-    // Part 3 - Player
-    printf("======================================= Part 3 =======================================\n");
-    PlayerDriver playerDriver;
-    printf("======================================= Part 3 end =======================================\n");
-    
-    // Part 4 - OrdersList
-    printf("======================================= Part 4 =======================================\n");
-    OrdersDriver orderDriver;
-    printf("======================================= Part 4 end =======================================\n");
-    
-    //Part 5 - Cards
-    printf("======================================= Part 5  =======================================\n");
-    CardsDriver cardDriver;
-    printf("======================================= Part 5 end =======================================\n");
-    
-    auto result = _getch();
-    return 0;
+	// Part 3 - Player
+	printf("======================================= Part 3 =======================================\n");
+	PlayerDriver playerDriver;
+	printf("======================================= Part 3 end =======================================\n");
+
+	// Part 4 - OrdersList
+	printf("======================================= Part 4 =======================================\n");
+	OrdersDriver orderDriver;
+	printf("======================================= Part 4 end =======================================\n");
+
+	//Part 5 - Cards
+	printf("======================================= Part 5  =======================================\n");
+	CardsDriver cardDriver;
+	printf("======================================= Part 5 end =======================================\n");
+	*/
+
+	//Part 1 - Game Start 
+	printf("======================================= Part 1  =======================================\n");
+	std::string path = "../WarzoneCpp";
+	for (const auto& entry : fs::directory_iterator(path)) {
+		if (entry.path().extension() == ".map") {
+			std::cout << entry.path() << std::endl;
+		}
+	}
+
+	printf("======================================= Part 1 end =======================================\n");
+
+	printf("hi");
+	auto result = _getch();
+	return 0;
 }
