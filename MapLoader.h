58a89a835c35a33b79b6f3312b18225dcee0eff4
ivanivachenko <46464724+ivanivachenko@users.noreply.github.com>
2020-11-04 20:18:19 -0500

    copy constructor and operator overloads added

--- MapLoader.h
+++ MapLoader.h
@@ -8,11 +8,18 @@ public:
 	~MapLoader();
 	void loadmap(std::string file);
 	bool getContinentCheck();
-
+	//copy constructor
+	MapLoader(const MapLoader& ml1);
+	//Assignment operator
+	void operator=(const MapLoader& ml);
+	friend std::ostream& operator<<(std::ostream& out, const MapLoader& ml);
+	friend std::istream& operator>>(std::istream& in, MapLoader& ml);
 private:
 	bool continentCheck;
 	bool countryCheck;
 	bool borderCheck;
 	bool mapCheck;
+	int ID;
 };
 #endif //!map
+
